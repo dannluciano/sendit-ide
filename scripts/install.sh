f5bb78d2d09fb11a4bd5fb5efea5e6a3bfc946de
Dann Luciano <dannluciano@gmail.com>
2021-10-26 15:51:32 -0300

    Close #2

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,8 +11,8 @@ echo "===> Building Docker VM Image"
 docker build -f docker/vm/Dockerfile -t sendit-ide_vm ..
 
 echo "===> Instaling ttyd"
-cp ../bin/ttyd /usr/bin/
-cp ../configs/ttyd.service /etc/systemd/system/ttyd.service
+cp ./bin/ttyd /usr/bin/
+cp ./configs/ttyd.service /etc/systemd/system/ttyd.service
 
 echo "===> Updating Systemd Unit"
 systemctl daemon-reload

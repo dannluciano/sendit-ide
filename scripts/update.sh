3c80ea24d1aecc1fd40ffd303f25b2e357743b15
Dann Luciano <dannluciano@gmail.com>
2021-10-26 15:56:37 -0300

    Fix previous commit

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,11 +11,11 @@ echo "===> Building Docker VM Image"
 docker build -f docker/vm/Dockerfile -t sendit-ide_vm ..
 
 echo "===> Instaling ttyd"
-cp ./bin/ttyd /usr/bin/
+# cp ./bin/ttyd /usr/bin/
 cp ./configs/ttyd.service /etc/systemd/system/ttyd.service
 
 echo "===> Updating Systemd Unit"
 systemctl daemon-reload
-systemctl enable ttyd
+# systemctl enable ttyd
 systemctl restart ttyd
 systemctl restart nginx

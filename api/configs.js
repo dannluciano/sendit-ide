315e68c4f45cef6d2c1f32ba6b448c73c16cf327
Dann Luciano <dannluciano@gmail.com>
2024-04-15 15:45:55 -0300

    Enhanced CSS

--- api/configs.js
+++ api/configs.js
@@ -1,7 +1,15 @@
+const DATABASE_PROD_PATH = "/var/storages/sendit-ide/sendit-ide.sqlite3";
+const DATABASE_DEV_PATH = "sendit-ide.sqlite3";
+let DATABASE_PATH = DATABASE_DEV_PATH;
+
+if (process.env["ENV"] == "production") {
+  DATABASE_PATH = DATABASE_PROD_PATH;
+}
+
 export default {
   DOCKER_ENGINE_SOCKET: {
     socketPath: "/var/run/docker.sock",
   },
   PORT: process.env["PORT"] || 8001,
-  DATABASE_NAME: '/var/storages/sendit-ide/sendit-ide.sqlite3'
+  DATABASE_NAME: DATABASE_PATH,
 };

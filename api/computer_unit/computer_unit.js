20d4ca1f5365c239dd6e141e28e5713877c6e8ab
Dann Luciano <dannluciano@gmail.com>
2024-05-07 22:41:08 -0300

    Add JSDoc to Computer Unit

--- api/computer_unit/computer_unit.js
+++ api/computer_unit/computer_unit.js
@@ -1,4 +1,19 @@
+import Dockerode from "dockerode";
+
+/**
+ * @typedef {Object} ComputerUnitJSON
+ * @property {string} container-id
+ * @property {string} temp-dir-path
+ * @property {string} project-id
+ */
+
 export default class ComputerUnit {
+  /**
+   *
+   * @param {Dockerode.Container} containerInstance
+   * @param {string} tempDirPath
+   * @param {string} projectId
+   */
   constructor(containerInstance, tempDirPath, projectId) {
     if (containerInstance) {
       this.id = containerInstance.id;
@@ -7,15 +22,16 @@ export default class ComputerUnit {
     }
     this.tempDirPath = tempDirPath;
     this.projectId = projectId;
-    this.ws = null;
   }
 
+  /**
+   * @returns {ComputerUnitJSON}
+   */
   toJSON() {
     return {
       "container-id": this.containerId,
       "temp-dir-path": this.tempDirPath,
       "project-id": this.projectId,
-      ws: this.ws,
     };
   }
 }
